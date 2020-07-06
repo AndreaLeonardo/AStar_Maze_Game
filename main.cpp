64ff932d997ad8428b27037232b60cf85c7b2e89
AndreaLeonardo <andrea.leonardo@stud.unifi.it>
2020-07-06 10:17:56 +0200

    link SFML and create a window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include "findpath.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    sf::RenderWindow window(sf::VideoMode(640, 640), "ASTAR ALGORITHM");
+
+    float deltaTime = 0.0f;
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+
+        deltaTime = clock.restart().asSeconds();
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
+
+    }
+
+
 }
